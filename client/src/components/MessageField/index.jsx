cc23c1b62f62f0b7ed1e895db8de3e2011a2c947
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-06-07 20:52:49 -0300

    update messages container layout

--- client/src/components/MessageField/index.jsx
+++ client/src/components/MessageField/index.jsx
@@ -1,6 +1,10 @@
 import { io } from "socket.io-client";
 import { useEffect, useState } from "react";
 
+import {BiSend} from "react-icons/bi"
+
+import styles from "./styles.module.css"
+
 
 const apiUrl = process.env.REACT_APP_API_URL;
 const socket = io(apiUrl);
@@ -28,14 +32,16 @@ export default function MessageField() {
   }
 
   return (
-    <div>
-      <div>
+    <div className={styles.messageField}>
+      <div className={styles.messageField__top}>
         <h2>Messages</h2>
       </div>
-      <div>{response}</div>
-      <div>
+      <div className={styles.messageField__messagesContainer}>
+        {response}
+        </div>
+      <div className={styles.messageField__messageInput}>
         <input type="text" placeholder="message" onChange={(event) => { setMessage(event.target.value) }} />
-        <button onClick={() => { sendMessage() }}>Click em mim</button>
+        <button onClick={() => { sendMessage() }}><BiSend/></button>
       </div>
     </div>
   )
--- client/src/components/UserListCard/index.jsx
+++ client/src/components/UserListCard/index.jsx
@@ -1,6 +1,8 @@
+import styles from "./styles.module.css"
+
 export default function ({ name, email }) {
     return (
-        <li>
+        <li className={styles.userCard}>
             <div>
                 <h3>{name}</h3>
                 <p>{email}</p>
