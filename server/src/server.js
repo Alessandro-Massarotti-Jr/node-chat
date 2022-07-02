f7cf9d2b184377ac1318d0f49620dffe4417a2f8
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-07-02 12:27:26 -0300

    Cors

--- server/src/server.js
+++ server/src/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 const server = http.createServer(app)
-const io = new Server(server,{cors: {origin: "*"}});
+const io = new Server(server,{cors: {origins:["*"] }});
 
 
 
