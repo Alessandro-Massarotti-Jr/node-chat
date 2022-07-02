2c8ad5cc1db567782c9ab4dff3a2ead627e529ae
Alessandro-Massarotti-jr <alemassajr@hotmail.com>
2022-07-02 13:18:03 -0300

    CORS Update

--- server/src/server.js
+++ server/src/server.js
@@ -12,10 +12,7 @@ const app = express();
 app.use(cors({origin: `${process.env.CORS_DOMAINS}`,}));
 app.use(express.json());
 const server = http.createServer(app)
-const io = new Server(server,{cors: {
-    origin: `${process.env.CORS_DOMAINS}`,
-    methods:["GET","POST"]
-}});
+const io = new Server(server,{cors: {origin: `${process.env.CORS_DOMAINS}`}});
 
 
 
